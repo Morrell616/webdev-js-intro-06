36f701bdf3b07f235dded131545cc9ff1f9b7950
Jen Morrell <morrell616@hotmail.com>
2024-12-31 16:06:50 -0500

    added 3 loops to assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -9,11 +9,21 @@ function evenOrOdd() {
     const num = 3;
     // Write the logic to decide if the variable "num" is even or odd
     // and set the element's value the string "Even" or "Odd" accordingly
-
+   if (num % 2 === 0) {
+    evenOrOddElement.textContent = "Even";
+   } else {
+   evenOrOddElement.textContent = "Odd";
+   }
+   
 }
 
 function sumTheNumbers() {
     let sum = 0;
+    for (let i =1; i <= 10; i++) {
+        sum += i;
+    }
+    sumTheNumbersElement.textContent =  sum;
+
     // Write the logic to sum the numbers 1 through 10
     // using a for loop. Check the expected output
     // on the assignment page
@@ -22,6 +32,14 @@ function sumTheNumbers() {
 
 function createNumberArray() {
     const numberArray = [];
+    let i = 1;
+    while (i <= 10) {
+        numberArray.push(i);
+        i++;
+    }
+    createNumberArrayElement.textContent = numberArray.join(", ");
+
+
 
     // Write the logic that loops 10 times and adds the value
     // to numberArray each iteration. Check the expected output
@@ -30,8 +48,9 @@ function createNumberArray() {
 }
 
 function render() {
-    // Call the created functions
-
+ evenOrOdd();
+ sumTheNumbers();
+ createNumberArray();
 }
 
 submissionBtn.addEventListener("click", function () {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+/*"use strict"
+
+for (let i = 0; i<10; i++) {
+    console.log(i);
+}
+
+let x = 0;
+while (x < 8) {
+    console.log(x);
+    x++;
+}
+let r = 5;
+do {
+    console.log(r);
+    r++;
+
+} while (r < 15);
+/*
